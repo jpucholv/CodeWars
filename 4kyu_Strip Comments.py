c0cf1cf581c2f8d59e2a3993872746ca55418977
jpucholv <96960258+jpucholv@users.noreply.github.com>
2022-05-04 22:08:57 +0200

    Strip_comments_Complete

---
+++ 4kyu_Strip Comments.py
@@ -0,0 +1,45 @@
+"""
+Complete the solution so that it strips all text that follows any of a set of comment markers passed in. Any whitespace at the end of the line should also be stripped out.
+
+Example:
+
+Given an input string of:
+
+apples, pears # and bananas
+grapes
+bananas !apples
+The output expected would be:
+
+apples, pears
+grapes
+bananas
+The code would be called like so:
+
+result = solution("apples, pears # and bananas\ngrapes\nbananas !apples", ["#", "!"])
+# result should == "apples, pears\ngrapes\nbananas"
+"""
+
+def solution(string, markers):
+    strList = string.splitlines()
+    index = 0
+
+    print(strList)
+
+    for str in strList:
+        for mark in markers:
+            if str.find(mark) != -1:
+                str = str[:str.find(mark)].strip()
+        strList[index] = str
+        index += 1
+
+    return '\n'.join(strList)
+
+'''
+Best solution:
+
+def solution(string,markers):
+    parts = string.split('\n')
+    for s in markers:
+        parts = [v.split(s)[0].rstrip() for v in parts]
+    return '\n'.join(parts)
+'''
